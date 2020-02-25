43a57e77dc5ff8b84e531c43546281b24b78f24f
Andrew Jones <andrew@andrew-jones.com>
2020-02-25 14:45:06 +0000

    Add basic operator tests

---
+++ tests/operators/test_dbt_operator.py
@@ -0,0 +1,42 @@
+import datetime
+from unittest import TestCase, mock
+from airflow import DAG, configuration
+from airflow_dbt.hooks.dbt_hook import DbtCliHook
+from airflow_dbt.operators.dbt_operator import DbtRunOperator, DbtTestOperator, DbtDepsOperator
+
+
+class TestDbtOperator(TestCase):
+    def setUp(self):
+        configuration.conf.load_test_config()
+        args = {
+            'owner': 'airflow',
+            'start_date': datetime.datetime(2020, 2, 27)
+        }
+        self.dag = DAG('test_dag_id', default_args=args)
+
+    @mock.patch.object(DbtCliHook, 'run_cli')
+    def test_dbt_run(self, mock_run_cli):
+        operator = DbtRunOperator(
+            task_id='run',
+            dag=self.dag
+        )
+        operator.execute(None)
+        mock_run_cli.assert_called_once_with('run')
+
+    @mock.patch.object(DbtCliHook, 'run_cli')
+    def test_dbt_test(self, mock_run_cli):
+        operator = DbtTestOperator(
+            task_id='test',
+            dag=self.dag
+        )
+        operator.execute(None)
+        mock_run_cli.assert_called_once_with('test')
+
+    @mock.patch.object(DbtCliHook, 'run_cli')
+    def test_dbt_deps(self, mock_run_cli):
+        operator = DbtDepsOperator(
+            task_id='deps',
+            dag=self.dag
+        )
+        operator.execute(None)
+        mock_run_cli.assert_called_once_with('deps')
