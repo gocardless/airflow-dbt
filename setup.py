32ea3d1da79efec9b95925424ee1c5101a0cba1c
Andrew Jones <andrew@andrew-jones.com>
2020-02-25 14:23:14 +0000

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -16,4 +16,17 @@ setup(
     long_description_content_type='text/markdown',
     license = 'MIT',
     url = 'https://github.com/gocardless/airflow-dbt',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
