a89d3b93473e7042225310c93e25be74f30d1f74
Andrew Jones <andrew@andrew-jones.com>
2020-02-26 14:51:24 +0000

    Remove untested Python versions from classifiers

--- setup.py
+++ setup.py
@@ -31,8 +31,6 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX :: Linux',
 
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
     ],
 )
