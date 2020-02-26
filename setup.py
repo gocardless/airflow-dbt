13e03f99f0c81f5338c09222c940468990bfe0ae
Andrew Jones <andrew@andrew-jones.com>
2020-02-26 09:46:29 +0000

    Load the version from __version__
    As per https://github.com/navdeep-G/setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 import codecs
+import os
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, 'airflow_dbt', '__version__.py')) as f:
+    exec(f.read(), about)
 
 with codecs.open('README.md', 'r', 'utf-8') as file:
     long_description = file.read()
 
 setup(
     name = 'airflow_dbt',
-    version = '1.0.0',
+    version = about['__version__'],
     packages = find_packages(exclude=['tests']),
     install_requires = ['apache-airflow == 1.10.6'],
     author = 'GoCardless',
