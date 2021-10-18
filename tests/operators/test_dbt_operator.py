f2fb0188a14559ab49654c353f874e60700193c7
Daniel Iñigo <daniel.inigo@datatonic.com>
2021-10-18 13:02:23 +0200

    Test GCP Cloud Build hook

--- tests/operators/test_dbt_operator.py
+++ tests/operators/test_dbt_operator.py
@@ -1,8 +1,10 @@
 import datetime
 from unittest import TestCase, mock
+from unittest.mock import patch
 
 from airflow import DAG, configuration
 
+from airflow_dbt.hooks.dbt_google_hook import DbtCloudBuildHook
 from airflow_dbt.hooks.dbt_hook import DbtCliHook
 from airflow_dbt.operators.dbt_operator import (
     DbtDepsOperator, DbtRunOperator, DbtSeedOperator, DbtSnapshotOperator,
@@ -63,3 +65,46 @@ class TestDbtCliOperator(TestCase):
         )
         operator.execute(None)
         mock_run_dbt.assert_called_once_with(['dbt', 'deps'])
+
+
+class TestDbtRunWithCloudBuild(TestCase):
+    def setUp(self):
+        configuration.conf.load_test_config()
+        args = {
+            'owner': 'airflow',
+            'start_date': datetime.datetime(2020, 2, 27)
+        }
+        self.dag = DAG('test_dag_id', default_args=args)
+
+    @patch('airflow_dbt.hooks.dbt_google_hook.CloudBuildHook')
+    @patch('airflow_dbt.hooks.dbt_google_hook.GCSHook')
+    def test_dbt_deps(self, MockLocalCloudBuildHook, MockGCSHook):
+        operator = DbtRunOperator(
+            task_id='test_dbt_run_on_cloud_build',
+            dbt_hook=DbtCloudBuildHook(
+                project_id='my-project-id',
+                gcp_conn_id='my_conn_id',
+                gcs_staging_location='gs://my-bucket/certain-folder/'
+            ),
+            dag=self.dag
+        )
+        operator.execute(None)
+        MockLocalCloudBuildHook.assert_called_once_with(gcp_conn_id='my_conn_id')
+        MockGCSHook.assert_called_once_with(gcp_conn_id='my_conn_id')
+        MockGCSHook().upload.assert_called_once()
+
+
+{
+    'steps': [{
+        'name': 'fishtownanalytics/dbt:0.21.0', 'entrypoint': '/bin/sh',
+        'args': ['-c', 'dbt', 'run'], 'env': []
+    }],
+    'source': {
+        'storageSource': {
+            'bucket': 'my-bucket',
+            'object':
+                'certain-folder/dbt_staging_c5013d9965b54386bcf84ab76e42c848'
+                '.tar.gz'
+        }
+    }
+}
