c58289919f274be55cd4b3b82a420fc525c4f036
Daniel Iñigo <daniel.inigo@datatonic.com>
2021-10-25 14:15:17 +0200

    Test Cloud Build DBT hook and remove test cloudbuild upload
    Also fix imports and fix a dbt cli test

--- tests/hooks/test_dbt_hook.py
+++ tests/hooks/test_dbt_hook.py
@@ -1,19 +1,20 @@
 from unittest import TestCase, mock
+from unittest.mock import patch
 
 from airflow.hooks.subprocess import SubprocessHook
 
-from airflow_dbt.hooks.dbt_hook import DbtCliHook
+from airflow_dbt.hooks.cli import DbtCliHook
+from airflow_dbt.hooks.google import DbtCloudBuildHook
 
 
-class TestDbtHook(TestCase):
+class TestDbtCliHook(TestCase):
     @mock.patch.object(SubprocessHook, 'run_command')
     def test_sub_commands(self, mock_run_command):
         hook = DbtCliHook()
         hook.run_dbt(['dbt', 'docs', 'generate'])
         mock_run_command.assert_called_once_with(
             command=['dbt', 'docs', 'generate'],
-            env={},
-            cwd='.',
+            env={}
         )
 
     def test_vars(self):
@@ -25,3 +26,37 @@ class TestDbtHook(TestCase):
 
         assert generated_command == ['dbt', 'run', '--vars',
             '{"foo": "bar", "baz": "true"}']
+
+
+class TestDbtCloudBuildHook(TestCase):
+    @patch('airflow_dbt.hooks.google.CloudBuildHook')
+    def test_create_build(self, MockCloudBuildHook):
+        mock_create_build = MockCloudBuildHook().create_build
+        hook = DbtCloudBuildHook(
+            project_id='test_project_id',
+            gcs_staging_location='gs://hello/file.tar.gz',
+            dbt_version='0.10.10',
+            env={'TEST_ENV_VAR': 'test'},
+            service_account='robot@mail.com'
+        )
+        hook.run_dbt(['docs', 'generate'])
+
+        expected_body = {
+            'steps': [{
+                'name': f'fishtownanalytics/dbt:0.10.10',
+                'args': ['docs', 'generate'],
+                'env': ['TEST_ENV_VAR=test']
+            }],
+            'source': {
+                'storageSource': {
+                    'bucket': 'hello',
+                    'object': 'file.tar.gz',
+                }
+            },
+            'serviceAccount': 'robot@mail.com'
+        }
+
+        mock_create_build.assert_called_once_with(
+            body=expected_body,
+            project_id='test_project_id'
+        )
--- tests/operators/test_dbt_operator.py
+++ tests/operators/test_dbt_operator.py
@@ -1,11 +1,9 @@
 import datetime
 from unittest import TestCase, mock
-from unittest.mock import patch
 
 from airflow import DAG, configuration
 
-from airflow_dbt.hooks.dbt_google_hook import DbtCloudBuildHook
-from airflow_dbt.hooks.dbt_hook import DbtCliHook
+from airflow_dbt.hooks.cli import DbtCliHook
 from airflow_dbt.operators.dbt_operator import (
     DbtDepsOperator, DbtRunOperator, DbtSeedOperator, DbtSnapshotOperator,
     DbtTestOperator,
@@ -75,29 +73,3 @@ class TestDbtRunWithCloudBuild(TestCase):
             'start_date': datetime.datetime(2020, 2, 27)
         }
         self.dag = DAG('test_dag_id', default_args=args)
-
-    @patch('airflow_dbt.hooks.dbt_google_hook.NamedTemporaryFile')
-    @patch('airflow_dbt.hooks.dbt_google_hook.CloudBuildHook')
-    @patch('airflow_dbt.hooks.dbt_google_hook.GCSHook')
-    def test_upload_files(self, MockGCSHook, MockCBHook, MockTempFile):
-        # Change the context provider returned name for the file
-        MockTempFile.return_value.__enter__.return_value.name = 'tempfile'
-        operator = DbtRunOperator(
-            task_id='test_dbt_run_on_cloud_build',
-            dbt_hook=DbtCloudBuildHook(
-                project_id='my-project-id',
-                gcp_conn_id='my_conn_id',
-                dir='.',
-                gcs_staging_location='gs://my-bucket/certain-folder'
-                                     '/stored_dbt_files.tar.gz'
-            ),
-            dag=self.dag
-        )
-        operator.execute(None)
-        MockCBHook.assert_called_once_with(gcp_conn_id='my_conn_id')
-        MockGCSHook.assert_called_once_with(gcp_conn_id='my_conn_id')
-        MockGCSHook().upload.assert_called_once_with(
-            bucket_name='my-bucket',
-            object_name='certain-folder/stored_dbt_files.tar.gz',
-            filename='tempfile'
-        )
